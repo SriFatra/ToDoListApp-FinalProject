2ec9850ee39ee7c444c2fc28a42cf47fe1e932d0
SriFatra <srirahayuindalfatra@gmail.com>
2020-05-28 07:23:08 +0800

    Add ToDoItem and ToDoItemDao

---
+++ app/src/main/java/com/srifatra/todolistapp/data/database/TodoItemDao.kt
@@ -0,0 +1,24 @@
+package com.srifatra.todolistapp.data.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface TodoItemDao {
+
+    @Insert
+    suspend fun saveTodoItem(todoItem: TodoItem)
+
+    @Insert
+    suspend fun saveTodoItems(todoItems: List<TodoItem>)
+
+    @Delete
+    suspend fun deleteTodoItem(todoItem: TodoItem)
+
+    @Update
+    suspend fun updateTodoItem(todoItem: TodoItem)
+
+    @Query("SELECT * FROM todo ORDER BY id DESC")
+    fun getAllTodoList(): LiveData<MutableList<TodoItem>>
+
+}
